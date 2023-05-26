ba9a6a2f6281b2eec37f25f386aead89d450a382
Sam <samturco42@gmail.com>
2023-05-25 21:08:52 -0700

    done but it isnt passing for some reason

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,38 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    print([i["name"] for i in spicy_foods])
+    return [i["name"] for i in spicy_foods]
+get_names(spicy_foods)
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    print([i for i in spicy_foods if i["heat_level"] > 5])
+    return [i for i in spicy_foods if i["heat_level"] > 5]
+get_spiciest_foods(spicy_foods)
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for i in spicy_foods:
+        x = f"{i['name']} ({i['cuisine']}) | Heat Level: {i['heat_level'] * '🌶'}"
+        print(x)
+print_spicy_foods(spicy_foods)
 
-def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+def get_spicy_food_by_cuisine(spicy_foods, x):
+    for i in spicy_foods:
+        if x == i['cuisine']:
+            return i
+get_spicy_food_by_cuisine(spicy_foods, "American")
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    x = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(x)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    print(sum([food["heat_level"] for food in spicy_foods]) / len(spicy_foods))
+    return sum([food["heat_level"] for food in spicy_foods]) / len(spicy_foods)
+get_average_heat_level(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+    
+# create_spicy_food(spicy_foods)
